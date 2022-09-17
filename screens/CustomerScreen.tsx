64daf07cabc533d2249e06148e89052d624338e1
Maaz Junior <goalmaaz@gmail.com>
2022-09-17 08:20:11 +0100

    search bar

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,9 +1,12 @@
-import { View, Text, SafeAreaView, Image, ScrollView} from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView, TextInput} from 'react-native'
 import {useNavigation, CompositeNavigationProp} from '@react-navigation/native';
 import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
 import { TabStackParamList } from '../navigator/TabNavigator';
 import { useTailwind } from 'tailwind-rn/dist'
-import React, {useLayoutEffect} from 'react'
+import React, {useLayoutEffect, useState} from 'react'
+
+
+
 
 export type CustomerScreenNavigationProp = CompositeNavigationProp<
 BottomTabNavigationProp<TabStackParamList, 'Customers'>
@@ -14,16 +17,23 @@ const CustomerScreen = () => {
   
     const tw = useTailwind();
     const navigation = useNavigation<CustomerScreenNavigationProp>()
+
+    const [input, setInput] = useState<string>("")
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
   })
   }, [])
   return (
-    <ScrollView>
-             <Image 
+    <ScrollView style={{backgroundColor: "#59c1cc"}}>
+
+      <Image 
       source={{uri: "https://links.papareact.com/3jc"}}
       style={tw("w-full h-64")}
+      />
+      <TextInput placeholder='Search by Customer'
+      value={input} onChangeText={setInput}
+      style={tw("bg-white pt-5 pb-5 px-10")}
       />
     </ScrollView>
       
